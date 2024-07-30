1475571d37db3a228e778a0251f9dc70e1ac02e2
Naveen U <142392636+Naveen00222@users.noreply.github.com>
2024-07-30 19:19:54 +0530

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,11 @@
-function pan () {
+function pan() {
     const pan = document.getElementById("pan").value;
-    const condition = /^[A-Z]{5}+\d{4}[A-Z]$/;
+    let condition = /^[A-Z]{5}\d{4}[A-Z]$/;
     if(pan.length>10){
         alert("pan number exceeded")
         return false
     }
-    else if(pan==null||pan===""){
+    else if(pan==null||pan==""){
         alert("Invalid pan Number")
     }              
     else if (condition.test(pan)) {
